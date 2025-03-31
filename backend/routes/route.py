ff3e34c6ec9a1f7214549137b957f4ecab5911e1
di0-nne <ddionneleow@gmail.com>
2025-03-31 19:02:29 +0800

    add tables schemas

--- backend/routes/route.py
+++ backend/routes/route.py
@@ -10,3 +10,23 @@ router = APIRouter()
 @router.get("/hawkerCentres")
 def hawkerCentres():
     return getHawkerCentres()
+
+@router.get("/cleaningSchedule")
+def cleaningSchedule():
+    return getCleaningSchedules()
+
+@router.get("/hawkerStalls")
+def hawkerStalls():
+    return getHawkerStalls()
+
+@router.get("/openingHours")
+def openingHours():
+    return getOpeningHours()
+
+@router.get("/reviews")
+def reviews():
+    return getReviews()
+
+# @router.get("/userHistory")
+# def userHistory():
+#     return getUserHistory()
